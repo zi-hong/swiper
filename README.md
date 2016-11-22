e66a4eaeb351a2851142f1c3b3439027cd90be22
zihong <972836897@qq.com>
2016-11-22 10:36:52 +0800

    增加竖直滚动

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ﻿移动端滚动插件
+
+支持amd加载
+
+相关参数：
+option = {
+	autoPlay: true,//是否自动播放
+	delay: 2000,//切换间隔时间(毫秒)
+	start: 0,//开始index
+	align: 'h', //or v 滚动方向 h水平  v竖直
+	isNav: true,//是否展示导航点
+	navColor: '#ffffff',//导航点默认颜色
+	navActiveColor: 'red',//导航点激活颜色
+	changeEvent: function(index, swpier) {}//切换事件
+}
--- swiper.js
+++ swiper.js
@@ -1,9 +1,9 @@
-(function(window, undefine) {
+(function(window, undefined) {
 
 	/**tool-start**/
 	function extend(orginObj, obj) {
 		for (var i in orginObj) {
-			orginObj[i] = obj[i] || orginObj[i];
+			orginObj[i] = obj[i] + '' !== 'undefined' ? obj[i] : orginObj[i];
 		}
 	}
 
@@ -16,224 +16,161 @@
 
 	function Swiper(el, op) {
 		//el是 querySelector
-		config = {
+		this.config = {
 				autoPlay: true,
 				delay: 2000,
 				start: 0,
+				align: 'h', //or v
 				isNav: true,
 				navColor: '#ffffff',
 				navActiveColor: 'red',
 				changeEvent: function(index, swpier) {}
 			}
 			/**合并参数**/
-		extend(config, op || {});
+		extend(this.config, op || {});
 
-		var currentDirect = 'left',
-			autoPlayTimer = null,
-			moveTimer = null,
-			navs = null;
+		this.currentDirect = 'left';
+		this.autoPlayTimer = null;
+		this.moveTimer = null;
+		this.navs = null;
 
 		this.element = document.querySelector(el);
-		swiperContent = document.querySelector(el).children[0];
-		swiperChild = swiperContent.children;
+		this.swiperContent = document.querySelector(el).children[0];
+		this.swiperChild = this.swiperContent.children;
 
 		this.element.style = 'position: relative;width: 100%;overflow: hidden;';
-		swiperContent.style = 'overflow: hidden;';
-		each(swiperChild, function(el) {
-			el.style = 'float: left;';
+		this.swiperContent.style = 'overflow: hidden;';
+
+		var _this = this;
+
+		if (this.config.isNav) {
+			this.singleWidth = window.getComputedStyle(this.element)['width'].match(/(\d+)/)[0];
+
+		} else {
+			this.singleWidth = window.getComputedStyle(this.element)['height'].match(/(\d+)/)[0];
+		}
+		each(this.swiperChild, function(el) {
+			if (_this.config.align == 'h') {
+				el.style = 'float: left;';
+			} else {
+				el.style = 'display:block;';
+			}
 		})
+		this.length = this.swiperChild.length;
+		this.allWidth = this.singleWidth * this.length;
 
-		singleWidth = window.getComputedStyle(this.element)['width'].match(/(\d+)/)[0];
-		this.length = swiperChild.length;
-		allWidth = singleWidth * this.length;
 
-		_this = this;
 
 		/**导航**/
 		(function() {
-			if (config.isNav && _this.length > 0) {
-				navs = document.createElement('div');
-				navs.style = 'pointer-events: none;text-align: center;position: absolute;width:100%;bottom:5px;';
+			if (_this.config.isNav && _this.length > 0) {
+				_this.navs = document.createElement('div');
+				_this.navs.style = 'pointer-events: none;text-align: center;position: absolute;width:100%;bottom:5px;';
 				var spans = '';
 				for (var i = 0; i < _this.length; i++) {
-					spans += '<span style="border-radius: 50%;display: inline-block;width: 10px;height: 10px;background-color: #' + config.navColor + ';margin-left:2px;margin-right:2px"></span>';
+					spans += '<span style="border-radius: 50%;display: inline-block;width: 10px;height: 10px;background-color: #' + _this.config.navColor + ';margin-left:2px;margin-right:2px"></span>';
 				}
-				navs.innerHTML = spans;
-				_this.element.appendChild(navs);
+				_this.navs.innerHTML = spans;
+				_this.element.appendChild(_this.navs);
 			}
 		})()
 
-		each(swiperChild, function(child) {
-			child.style.width = singleWidth + 'px';
+		each(this.swiperChild, function(child) {
+			if (_this.config.align == 'h') {
+				child.style.width = _this.singleWidth + 'px';
+			} else {
+				child.style.height = _this.singleWidth + 'px';
+			}
 		})
-
-		swiperContent.style.width = allWidth + 'px';
+		if (this.config.align == 'h') {
+			this.swiperContent.style.width = this.allWidth + 'px';
+		}
 
 		/**初始化位置**/
 		(function() {
-			setX(-config.start * singleWidth);
-			navSelect(config.start);
+			_this.setX(-_this.config.start * _this.singleWidth);
+			_this.navSelect(_this.config.start);
 		})()
 
-		var x = 0; //记录开始滑动的位置
+		var x = 0,
+			y = 0; //记录开始滑动的位置
 
-		swiperContent.addEventListener('touchmove', function(e) {
 
-			var dir = direction(x, e.targetTouches[0].pageX);
-			currentDirect = dir;
+		/**绑定事件**/
+		this.swiperContent.addEventListener('touchmove', function(e) {
+
+			var dir = '';
+			if (_this.config.align == 'h') {
+				dir = _this.direction(_this.x, e.targetTouches[0].pageX);
+			}else{
+				dir = _this.direction(_this.y, e.targetTouches[0].pageY);
+			}
+			_this.currentDirect = dir;
 			switch (dir) {
 				case 'left':
-					moveX(-Math.abs(e.targetTouches[0].pageX - x));
+					_this.moveX(-Math.abs(e.targetTouches[0].pageX - _this.x));
 					break;
 				case 'right':
-					moveX(Math.abs(e.targetTouches[0].pageX - x));
+					_this.moveX(Math.abs(e.targetTouches[0].pageX - _this.x));
 					break;
+				case 'up':
+					_this.moveX(-Math.abs(e.targetTouches[0].pageY - _this.y));
+					break;
+				case 'down':
+					_this.moveX(Math.abs(e.targetTouches[0].pageY - _this.y));
+					break;
+
 			}
-			x = e.targetTouches[0].pageX;
+			_this.x = e.targetTouches[0].pageX;
+			_this.y = e.targetTouches[0].pageY;
 		})
 
-		swiperContent.addEventListener('touchstart', function(e) {
+		this.swiperContent.addEventListener('touchstart', function(e) {
 
-			autoPlayTimer && clearInterval(autoPlayTimer);
-			moveTimer && clearInterval(moveTimer);
+			_this.autoPlayTimer && clearInterval(_this.autoPlayTimer);
+			_this.moveTimer && clearInterval(_this.moveTimer);
 
-			x = e.targetTouches[0].pageX;
+			_this.x = e.targetTouches[0].pageX;
+			_this.y = e.targetTouches[0].pageY;
 
 		})
 
-		swiperContent.addEventListener('touchend', function(e) {
+		this.swiperContent.addEventListener('touchend', function(e) {
 
-			x = 0;
+			_this.x = 0;
+			_this.y = 0;
 
-			var index = getIndex();
-			if (currentDirect == 'left') {
+			var index = _this.getIndex();
+			if (_this.currentDirect == 'left' || _this.currentDirect == 'up') {
 				index = index + 1 < _this.length ? index + 1 : _this.length - 1;
 			}
-			toMoveIndex(index);
+			_this.toMoveIndex(index);
 
-			if (config.autoPlay) {
+			if (_this.config.autoPlay) {
 				// setTimeout(function() {
-				autoPlay();
+				_this.autoPlay();
 				// }, 1000)
 			}
 		})
 
-
-
-		/**缓缓移动**/
-		function toMoveX(x) {
-			var i = 1;
-			var currentX = getCurrentX();
-			var value = currentX - x;
-			//label 用于判断方向
-			var label = '+';
-
-			if (value > 0) {
-				label = '-';
-			}
-			var step = parseInt(label + 20);
-
-			moveTimer && clearInterval(moveTimer);
-			moveTimer = setInterval(function() {
-
-				value = value + step;
-
-				moveX(step);
-
-				if (label == '-' && value <= 0) {
-					setX(x);
-					clearInterval(moveTimer);
-
-				} else if (label == '+' && value >= 0) {
-					setX(x);
-					clearInterval(moveTimer);
-
-				}
-
-				i += 5;
-
-			}, 20 + i);
+		if (this.config.autoPlay) {
+			this.autoPlay();
 		}
-
-		/**移动到相应的index**/
-		function toMoveIndex(index) {
-
-			var toX = -(index * singleWidth);
-
-			toMoveX(toX);
-			config.changeEvent(index, this);
-			navSelect(index);
-		}
-
-		function navSelect(index) {
-			var spans = navs.children;
-			for (var i = 0; i < spans.length; i++) {
-				if (index == i) {
-					spans[i].style.backgroundColor = config.navActiveColor;
-				} else {
-					spans[i].style.backgroundColor = config.navColor;
-				}
-			}
-		}
-
-		/**获得目前的index**/
-		function getIndex() {
-			var currentX = getCurrentX();
-
-			var step = 0;
-			if (!currentX) {
-				return step;
-			}
-
-			step = parseInt(Math.abs(currentX) / singleWidth);
-
-			return step;
-
-		}
-
-		/**获得当前位移**/
-		function getCurrentX() {
-			return parseInt(swiperContent.style.transform ? swiperContent.style.transform.match(/-*\d+/)[0] : 0);
-		}
-
-		/**实际的设置移动距离**/
-		function setX(tempx) {
-			swiperContent.style.transform = 'translateX(' + tempx + 'px)';
-		}
-
-		/**通过将移动的index算出位移量**/
-		function moveX(step) {
-
-			var currentX = getCurrentX();
-
-			var tempx = parseInt(currentX) + parseInt(step);
-
-			if (tempx >= 0) {
-				tempx = 0;
-			}
-			if (-tempx >= allWidth - singleWidth) {
-				tempx = -(allWidth - singleWidth);
-			}
-			setX(tempx);
-		}
-
-		/**判断方向**/
-		function direction(x1, x2) {
-			return x2 > x1 ? 'right' : 'left';
-		}
-
+	}
+	Swiper.prototype = {
 		/**自动播放**/
-		function autoPlay() {
-			var index = getIndex();
+		autoPlay: function() {
+			var _this = this;
+			var index = this.getIndex();
 
 			var flag = 2; //标记两个方向的滚动 1 index增加  2 index减少
 			if (index + 1 < _this.length) {
 				flag = 1;
 			}
 
-			autoPlayTimer && clearInterval(autoPlayTimer);
+			_this.autoPlayTimer && clearInterval(_this.autoPlayTimer);
 
-			autoPlayTimer = setInterval(function() {
+			this.autoPlayTimer = setInterval(function() {
 
 				if (flag == 1 && index + 1 < _this.length) {
 					index++;
@@ -247,16 +184,118 @@
 					index++;
 				}
 
-				toMoveIndex(index);
+				_this.toMoveIndex(index);
 
-			}, config.delay)
-		}
+			}, _this.config.delay)
+		},
+		/**判断方向**/
+		direction: function(x1, x2) {
+			if (this.config.align == 'h') {
+				return x2 > x1 ? 'right' : 'left';
+			} else {
+				return x2 > x1 ? 'down' : 'up';
+			}
+		},
+		/**缓缓移动**/
+		toMoveX: function(x) {
+			var i = 1;
+			var currentX = this.getCurrentX();
+			var value = currentX - x;
+			//label 用于判断方向
+			var label = '+';
 
-		if (config.autoPlay) {
-			autoPlay();
+			if (value > 0) {
+				label = '-';
+			}
+			var step = parseInt(label + this.singleWidth / 20);
+			var _this = this;
+			this.moveTimer && clearInterval(this.moveTimer);
+			this.moveTimer = setInterval(function() {
+
+				value = value + step;
+
+				_this.moveX(step);
+
+				if (label == '-' && value <= 0) {
+					_this.setX(x);
+					clearInterval(_this.moveTimer);
+
+				} else if (label == '+' && value >= 0) {
+					_this.setX(x);
+					clearInterval(_this.moveTimer);
+
+				}
+
+				i += 5;
+
+			}, 20 + i);
+		},
+		/**实际的设置移动距离**/
+		setX: function(tempx) {
+			if (this.config.align == 'h') {
+				this.swiperContent.style.transform = 'translateX(' + tempx + 'px)';
+			} else {
+				this.swiperContent.style.transform = 'translateY(' + tempx + 'px)';
+			}
+		},
+
+		/**通过将移动的index算出位移量**/
+		moveX: function(step) {
+
+			var currentX = this.getCurrentX();
+
+			var tempx = parseInt(currentX) + parseInt(step);
+
+			if (tempx >= 0) {
+				tempx = 0;
+			}
+			if (-tempx >= this.allWidth - this.singleWidth) {
+				tempx = -(this.allWidth - this.singleWidth);
+			}
+			this.setX(tempx);
+		},
+		/**获得当前位移**/
+		getCurrentX: function() {
+			return parseInt(this.swiperContent.style.transform ? this.swiperContent.style.transform.match(/-*\d+/)[0] : 0);
+		},
+		/**移动到相应的index**/
+		toMoveIndex: function(index) {
+
+			var toX = -(index * this.singleWidth);
+
+			this.toMoveX(toX);
+			this.config.changeEvent(index, this);
+			this.navSelect(index);
+		},
+		navSelect: function(index) {
+			if (this.config.isNav) {
+				var spans = this.navs.children;
+				for (var i = 0; i < spans.length; i++) {
+					if (index == i) {
+						spans[i].style.backgroundColor = this.config.navActiveColor;
+					} else {
+						spans[i].style.backgroundColor = this.config.navColor;
+					}
+				}
+			}
+		},
+		/**获得目前的index**/
+		getIndex: function() {
+			var currentX = this.getCurrentX();
+
+			var step = 0;
+			if (!currentX) {
+				return step;
+			}
+
+			step = parseInt(Math.abs(currentX) / this.singleWidth);
+
+			return step;
+
 		}
 	}
 
+	/**兼容amd**/
 	if (typeof define === 'function' && define.amd) {
 		define(function() {
 			return Swiper;
